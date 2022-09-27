555abce69d341a310ea9d8d4b898d5f4515a3323
KreoX <soldimksol@gmail.com>
2022-09-27 13:30:32 +0300

    Ex2Mod

--- Ex2.py
+++ Ex2.py
@@ -1,12 +1,16 @@
 x = int(input('Введите координату х: '))
 y = int(input('Введите координату y: '))
-if x == 0 or y == 0:
+if x == 0 and y == 0:
     print('Неверные координаты!')
 elif x > 0 and y > 0:
-    print('1')
+    print('1 четверть')
 elif x < 0 and y > 0:
-    print('2')
+    print('2  четверть')
 elif x < 0 and y < 0:
-    print('3')
+    print('3  четверть')
+elif x > 0 and y < 0:
+    print('4 четверть')
+elif x==0:
+    print('Ось Х')
 else:
-    print('4')
+    print('Ось Y')
